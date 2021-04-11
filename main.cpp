19b3aef791b18608d47c6936f985bf271cccc050
Elias Nicolas <Dakotathoughs@gmail.com>
2021-04-11 02:06:10 -0300

    Nothing wrong with starting over

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+// Un docente desea saber qué porcentaje de hombres y qué porcentaje de mujeres hay en un grupo de alumnos.
+// Ejemplo: Ingrese cantidad de mujeres: 30
+// Ingrese cantidad de varones: 20
+// En la comisión hay un 60% de mujeres y un 40% de
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
