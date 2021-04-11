317e8919fe1060aad1bb655a4c2ba4df5c4ad0c3
Elias Nicolas <Dakotathoughs@gmail.com>
2021-04-11 02:22:42 -0300

    Starting over

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+// Dadas dos variables numéricas a y b,que el usuario debe ingresar,
+// se pide realizar un algoritmo que intercambielos valores de ambas
+// variables y muestre cuánto valen ambas al final. Ejemplo:
+// Ingrese valor de a: 130
+// Ingrese valor de b: 45
+// --Intercambiamos a y b
+// --a: 45
+// --b: 130
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
