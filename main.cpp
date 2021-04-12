3e30ea07421e0f61902b19760bcd97f88ddac703
Elias Nicolas <Dakotathoughs@gmail.com>
2021-04-12 19:18:05 -0300

    Starting over

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int nroLegajo;
+    cout <<"Me dices tu número de legajo?"<< endl;
+    cin >> nroLegajo;
+    cout <<"Ingreso de "<< nroLegajo <<" al Zinjai!"<< endl;
+    return 0;
+
+}
