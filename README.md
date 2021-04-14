6e9a2f6f318fc471605d393acd472cf44b6b1c59
Elias Nicolas <Dakotathoughs@gmail.com>
2021-04-14 07:44:33 -0300

    Fixed readme again

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # AEDD
-AEDD UTN
+Un docente desea saber qué porcentaje de hombres y qué porcentaje de mujeres hay en un grupo de alumnos.
+Ejemplo: Ingrese cantidad de mujeres: 30
+Ingrese cantidad de varones: 20
+En la comisión hay un 60% de mujeres y un 40% de varones. 
--- main.cpp
+++ main.cpp
@@ -1,8 +1,5 @@
 #include <iostream>
-// Un docente desea saber qué porcentaje de hombres y qué porcentaje de mujeres hay en un grupo de alumnos.
-// Ejemplo: Ingrese cantidad de mujeres: 30
-// Ingrese cantidad de varones: 20
-// En la comisión hay un 60% de mujeres y un 40% de
+
 int main() {
     std::cout << "Hello, World!" << std::endl;
     return 0;
